75c79c2c04523bfe47356dcead7f00762aebb2b7
Michal Sousedík <sousemic@fit.cvut.cz>
2020-02-29 21:52:46 +0100

    Navigation Bar

--- App/milacci/Milacci/AppDelegates/MainAppDelegate.swift
+++ App/milacci/Milacci/AppDelegates/MainAppDelegate.swift
@@ -8,11 +8,6 @@ final class MainAppDelegate: UIResponder, UIApplicationDelegate {
     private lazy var appFlowCoordinator = AppFlowCoordinator()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.makeKeyAndVisible()
-
-        // swiftlint:disable force_unwrapping
-        appFlowCoordinator.start(in: window!)
         return true
     }
 }
