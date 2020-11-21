ac166f39f1233c079b6ae30e033a7f127707c5e6
Michal Sousedik <michal.sousedik@gmail.com>
2020-11-22 00:43:34 +0100

    Hour Rate Stats Coordinator

---
+++ App/milacci/Milacci/Module/HourRateStats/Coordinator/HourRateStatsCoordinator.swift
@@ -0,0 +1,37 @@
+//
+//  HourRateStatsCoordinator.swift
+//  Milacci
+//
+//  Created by Michal Sousedik on 20/11/2020.
+//  Copyright © 2020 Michal Sousedik. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SFSafeSymbols
+
+class HourRateStatsCoordinator: BaseCoordinator {
+
+    init(navigationController: UINavigationController) {
+        super.init()
+        self.navigationController = navigationController
+    }
+
+    override func start(){
+        let view = HourRateStatsViewController.instantiate()
+        view.viewModelBuilder = {
+            HourRateStatsViewModel(input: $0,
+                                   dependencies: (
+                                    userAPI: UserService.shared,
+                                    hourRateAPI: HourRateService.shared))
+        }
+        view.tabBarItem = UITabBarItem(title: L10n.hourRateStatistics, image: UIImage(systemSymbol: .chartBarFill), tag: 0)
+        navigationController.pushViewController(view, animated: true)
+        setupNavigationBar()
+    }
+
+    func setupNavigationBar(){
+        navigationController.navigationBar.prefersLargeTitles = true
+    }
+
+}
